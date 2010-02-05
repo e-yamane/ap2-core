31baf2716b6c2374095a00c82c85698e60ee95a3
e-yamane <e-yamane@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2010-02-05 01:22:35 +0000

    Placeの階層化をこのレイヤで取りやめる。（フラットにする） 本修正でgetTrend、getBalanceの場所grossは行われなくなる。 今後どーするかは要検討。
    個人的には、AP-MasterとAP-Tranは分離するのかなぁ・・・
    
    git-svn-id: https://dev.rough-diamond.jp/svn/framework/accountPattern2/core/trunk/ap2-core@902 32b2b61f-7f20-47f8-a8dc-572efa4bece0

--- src/java/jp/rough_diamond/account/entity/Place.java
+++ src/java/jp/rough_diamond/account/entity/Place.java
@@ -1,12 +1,7 @@
 package jp.rough_diamond.account.entity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,9 +13,7 @@ import jp.rough_diamond.commons.extractor.Max;
 import jp.rough_diamond.commons.extractor.Order;
 import jp.rough_diamond.commons.extractor.Property;
 import jp.rough_diamond.commons.service.BasicService;
-import jp.rough_diamond.commons.service.annotation.PostLoad;
 import jp.rough_diamond.commons.service.annotation.PreUpdate;
-import jp.rough_diamond.framework.transaction.TransactionManager;
 
 /**
  * 場所のHibernateマッピングクラス
@@ -52,17 +45,6 @@ public class Place extends jp.rough_diamond.account.entity.base.BasePlace {
     	return Math.max(ret, (Long)BasicService.getService().findByExtractor(ex2).get(0));
     }
 
-	public List<Place> getRoutes() {
-    	List<Place> list = new ArrayList<Place>();
-    	Place parent = getParent();
-    	while(parent != null) {
-    		list.add(parent);
-    		parent = parent.getParent();
-    	}
-    	Collections.reverse(list);
-    	return list;
-    }
-    
     public static Set<Long> getAllIds() {
     	Extractor e = new Extractor(Place.class);
     	e.addExtractValue(new ExtractValue("id", new Property(Place.class, null, Place.ID)));
@@ -74,61 +56,6 @@ public class Place extends jp.rough_diamond.account.entity.base.BasePlace {
     	return ret;
     }
     
-    public List<Place> getChildren() {
-    	Extractor extractor = new Extractor(Place.class);
-    	extractor.add(Condition.eq(new Property(PARENT), this));
-    	extractor.addOrder(Order.asc(new Property(ID)));
-    	return BasicService.getService().findByExtractor(extractor);
-    }
-
-	public Set<Long> getChildIds() {
-    	Set<Long> set = new HashSet<Long>();
-    	findChildIds(set, Arrays.asList(new Long[]{getId()}));
-    	return set;
-	}
-
-    private static void findChildIds(Set<Long> set, Collection<Long> ids) {
-    	Extractor e = new Extractor(Place.class);
-    	e.add(Condition.in(new Property(PARENT + "." + ID), ids));
-    	e.addExtractValue(new ExtractValue("id", new Property(Place.class, null, ID)));
-    	Set<Long> tmp = new HashSet<Long>();
-    	List<Map<String, Long>> list = BasicService.getService().findByExtractor(e);
-    	for(Map<String, Long> map : list) {
-    		tmp.add(map.get("id"));
-    	}
-    	removeCycricIds(set, tmp);
-    	set.addAll(tmp);
-    	if(tmp.size() != 0) {
-    		findChildIds(set, tmp);
-    	}
-    }
-    
-    private static void removeCycricIds(Set<Long> baseIds, Set<Long> newIds) {
-    	Iterator<Long> ite = newIds.iterator();
-    	while(ite.hasNext()) {
-    		Long id = ite.next();
-    		if(baseIds.contains(id)) {
-    			ite.remove();
-    		}
-    	}
-    }
-    
-    private final static String SKIP_LOAD_PARENT_KEY = "jp.rough_diamond.account.entity.Plance.loadParents.skip";
-    public static void skipLoadParents() {
-    	Map<Object, Object> map = TransactionManager.getTransactionContext();
-    	if(map != null) {
-    		map.put(SKIP_LOAD_PARENT_KEY, Boolean.TRUE);
-    	}
-    }
-    
-    @PostLoad
-    @Override
-    public void loadParents() {
-    	if(!TransactionManager.getTransactionContext().containsKey(SKIP_LOAD_PARENT_KEY)) {
-    		super.loadParents();
-    	}
-    }    
-
 	public MasterStatus getStatus() {
 		return MasterStatus.getStstusByCode(getStatusCode());
 	}
